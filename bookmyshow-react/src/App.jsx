7f26e26a21d7405a9ca54676be2721bb6cb398fc
srikanth <srikanth4769yadav@gmail.com>
2025-10-23 19:31:55 +0530

    display movies

--- bookmyshow-react/src/App.jsx
+++ bookmyshow-react/src/App.jsx
@@ -1,14 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const movies = [
+    {
+      id: 1,
+      title: "Telusu Kadha",
+      rating: 9,
+      location: "Bangalore",
+      icon: "🎬"
+    },
+    {
+      id: 2,
+      title: "Kantara",
+      rating: 10,
+      location: "Hyderabad",
+      icon: "🔥"
+    },
+    {
+      id: 3,
+      title: "OG",
+      rating: 8,
+      location: "Vizag",
+      icon: "💪"
+    },
+    {
+      id: 4,
+      title: "K-Ramp",
+      rating: 8.5,
+      location: "Guntur",
+      icon: "💪"
+    },
+    {
+      id: 5,
+      title: "Mitramandali",
+      rating: 7,
+      location: "Vijayawada",
+      icon: "💪"
+    }
+  ];
 
   return (
     <>
-      srikanth
+      <h1>🎬 Recommended Movies </h1>
+      <div className='container'>
+        {movies.map(movie => (
+          <div className='movie-div'>
+            <p className='icon'>{movie.icon}</p>
+            <h3 className='margin title'>{movie.title}</h3>
+            <p className='margin'>⭐ Rating: {movie.rating}</p>
+            <p className='margin'>📍 {movie.location}</p>
+          </div>
+        ))}
+      </div>
+
     </>
   )
 }
